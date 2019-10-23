8301412346120cc6b859c485d049b26613c90056
guruprakashchinnasamy <55480942+guruprakashchinnasamy@users.noreply.github.com>
2019-10-23 23:58:47 +0530

    Commit For yield

---
+++ Yield/ConsoleApp_Yield/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp_Yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            foreach (int v in hello(30))
+            {
+                Console.Write(v);
+                Console.Write(" ");
+            }
+            static IEnumerable<int> hello(int Loops)
+            {
+                for (int i = 0; i < Loops; i++)
+                {
+                    yield return i;
+                }
+
+            }
+        }
+    }
+}
